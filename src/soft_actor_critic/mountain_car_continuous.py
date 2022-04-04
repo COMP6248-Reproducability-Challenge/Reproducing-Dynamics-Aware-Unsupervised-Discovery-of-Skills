9925278626533e9777a1fdbe71ab937f069c0821
jwbirkbeck <29716383+jwbirkbeck@users.noreply.github.com>
2022-04-04 17:10:19 +0100

    adding soft actor critic files and two examples

---
+++ src/soft_actor_critic/mountain_car_continuous.py
@@ -0,0 +1,21 @@
+import torch
+import sys
+
+sys.path.append(r'/opt/project/dads_replication/src')
+sys.path.append(r'/opt/project/dads_replication/src/agent/')
+
+from agent.SACAgent import SACAgent
+from environments.mountaincar_cont import MountainCarContinuous
+
+env = MountainCarContinuous()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+agent = SACAgent(env=env, device=device, learning_rate=0.01)
+
+t = 0
+while agent.winstreak < 10 and t < 50:
+    t += 1
+    agent.play_games(1, verbose=True)
+
+agent.play_games(1, verbose=True, display_gameplay=True)
---
+++ src/soft_actor_critic/pendulum_continuous.py
@@ -0,0 +1,20 @@
+import torch
+import sys
+
+sys.path.append(r'/opt/project/dads_replication/src')
+sys.path.append(r'/opt/project/dads_replication/src/agent/')
+
+from agent.SACAgent import SACAgent
+from environments.pendulum import Pendulum
+
+env = Pendulum()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+agent = SACAgent(env=env, device=device)
+
+t = 0
+while agent.winstreak < 10 and t < 150:
+    t += 1
+    agent.play_games(1, verbose=True)
+
+agent.play_games(1, verbose=True, display_gameplay=True)
